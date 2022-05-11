9b61bb6af5d0ace9edc5bca7f1d00b22b5e3be3a
binogabriel <gabrielfv.almeida@gmail.com>
2022-05-11 19:04:11 -0300

    Gabriel

--- src/main/kotlin/ExercicioWhile.kt
+++ src/main/kotlin/ExercicioWhile.kt
@@ -1,14 +1,42 @@
-fun caixa(){
+fun caixa() {
     var balao = 0
     var i = 1
-    while (i<2000){
-        i+=7
+    while (i < 2000) {
+        i += 7
         balao++
     }
-    print(balao)
+    println(balao)
 }
 
+fun fizzbuzz() {
+    var i = 1
+    while (i <= 50) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            println("FizzBuzz")
+        } else if (i % 3 == 0) {
+            println("Buzz")
+        } else if (i % 5 == 0) {
+            println("Fizz")
+        } else println(i)
+        i++
+    }
+}
+
+fun inverte() {
+    print("Entre com o nome: ")
+    val str = readln()
+    var i = str.length
+    if (str != "") {
+        while (i > 0) {
+            val str1 = str[i - 1]
+            print(str1)
+            i--
+        }
+    }
+}
 
 fun main() {
     caixa()
+    fizzbuzz()
+    inverte()
 }
