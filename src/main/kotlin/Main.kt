2d5d93cb6e544367b35231613ec2a6b89114586b
binogabriel <gabrielfv.almeida@gmail.com>
2022-05-17 18:51:51 -0300

    Gabriel

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,37 @@
-fun main() {
-    /*for (i in 0..20 step 2){
-        print("$i ")
-    }*/
-    /*for (j in 20 downTo 0 ){
-        print("$j ")
-    }*/
-    /*
-    var s = "Gabriel"
-    var i = 0
-    while (i<s.length){
-        print("${s[i]} ")
-        i++
-    }*/
+class Pessoa(val anoNascimento: Int, var nome: String){
 
+    var olho: String = ""
+
+    var documento = ""
+    var doc: String? = null
+    var olhos: String? = null
+    constructor(anoNascimento: Int, nome: String, doc: String, olhos: String) : this(anoNascimento,nome){
+        this.doc = doc
+        this.olhos = olhos
+    }
+    fun dormir(){
+
+    }
+
+    fun acordar(){
+
+    }
+}
+
+
+fun main() {
+    // class - comportamentos e atributos
+
+    // classe (instancia) objetos
+    var mulher: Pessoa = Pessoa(2020,"asd","10804861609","pretos") // usa o construtor secundário
+    var homem: Pessoa = Pessoa(2000,"fulano") // usa o construtor primário
+
+    // this = se refere ao objeto
+
+    mulher.anoNascimento
+    mulher.nome
+    mulher.dormir()
+    mulher.acordar()
+    mulher.olhos = "preto"
+    println(mulher.olhos)
 }
